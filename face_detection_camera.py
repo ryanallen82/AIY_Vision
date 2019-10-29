bcb494f6e79a9815135a01838b8e6564fb17146c
Ryan Allen <allen.ryan@mayo.edu>
2019-10-29 14:41:25 -0500

    Added exception clause for x, y

--- face_detection_camera.py
+++ face_detection_camera.py
@@ -69,9 +69,12 @@ def main():
                     x, y, width, height = face.bounding_box
                 annotator.update()
 
-
-                print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f, x=%.2f, y=%.2f, width=%.2f, height=%.2f' %
-                    (inference.count, inference.rate, len(faces), avg_joy_score(faces), x, y, width, height))
+                if x:
+                    print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f, x=%.2f, y=%.2f, width=%.2f, height=%.2f' %
+                        (inference.count, inference.rate, len(faces), avg_joy_score(faces), x, y, width, height))
+                else:
+                    print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f' %
+                        (inference.count, inference.rate, len(faces), avg_joy_score(faces)))
 
         camera.stop_preview()
 
