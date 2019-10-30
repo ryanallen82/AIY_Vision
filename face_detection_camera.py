ad1f003e4ff73f4b98843cbcfbb73a6c22007f07
Ryan Allen <allen.ryan@mayo.edu>
2019-10-30 09:09:40 -0500

    asdf

--- face_detection_camera.py
+++ face_detection_camera.py
@@ -70,12 +70,13 @@ def main():
                 for face in faces:
                     annotator.bounding_box(transform(face.bounding_box), fill=None)
                     x, y, width, height = face.bounding_box
-                    camera.annotate_text('%.2f') % x
+
                 annotator.update()
 
                 if x:
                     print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f, x=%.2f, y=%.2f, width=%.2f, height=%.2f' %
                         (inference.count, inference.rate, len(faces), avg_joy_score(faces), x, y, width, height))
+                    camera.annotate_text('%.2f') % (x)
                 else:
                     print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f' %
                         (inference.count, inference.rate, len(faces), avg_joy_score(faces)))
