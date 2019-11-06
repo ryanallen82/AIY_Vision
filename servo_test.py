0f407c77115e9193bca4270b550307b877941844
ryanallen82 <ryan.michael.allen@gmail.com>
2019-11-06 12:40:51 -0600

    asdf

--- servo_test.py
+++ servo_test.py
@@ -10,8 +10,8 @@ def main():
     maxPW=(2.0+myCorrection)/1000
     minPW=(1.0-myCorrection)/1000
 
-    servo = Servo(PIN_A,min_pulse_width=minPW,max_pulse_width=maxPW)
-
+    #servo = Servo(PIN_A,min_pulse_width=minPW,max_pulse_width=maxPW)
+    servo = Servo(PIN_A)
     while True:
         servo.mid()
         print("mid")
