41d83e1e3128c14eb06e60360113956d9ad42a1a
ryanallen82 <ryan.michael.allen@gmail.com>
2019-11-06 12:18:31 -0600

    updating servo

--- servo_test.py
+++ servo_test.py
@@ -1,14 +1,20 @@
 from gpiozero import Servo
 from aiy.pins import PIN_A
+import time
 
 
 def main():
 
     #with PiCamera() as camera:
-    servo = Servo(PIN_A, min_pulse_width=.0001, max_pulse_width=.00025)
+    servo = Servo(PIN_A)
     servo.mid()
-    #camera.start_preview()
-    servo.value = 1
+    time.sleep(2)
+    servo.min()
+    time.sleep(2)
+    servo.mid()
+    time.sleep(2)
+    servo.max()
+    time.sleep(2)
     servo.mid()
 
 if __name__ == '__main__':
