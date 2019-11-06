bf3f1b35ee465fac3a9eddefa80e200f8967aa84
ryanallen82 <ryan.michael.allen@gmail.com>
2019-11-06 12:10:46 -0600

    fix

--- servo_test.py
+++ servo_test.py
@@ -3,5 +3,11 @@ from aiy.pins import PIN_A
 
 
 def main():
+
+    #with PiCamera() as camera:
     servo = Servo(PIN_A, min_pulse_width=.0001, max_pulse_width=.00025)
     servo.mid()
+    #camera.start_preview()
+
+if __name__ == '__main__':
+    main()
