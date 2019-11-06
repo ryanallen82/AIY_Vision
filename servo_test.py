4f76d63f2652a9497f160296a92b52a8751199b5
ryanallen82 <ryan.michael.allen@gmail.com>
2019-11-06 12:38:18 -0600

    Back to normal servo object

--- servo_test.py
+++ servo_test.py
@@ -5,20 +5,26 @@ import time
 
 def main():
 
+
     myCorrection=0.45
     maxPW=(2.0+myCorrection)/1000
     minPW=(1.0-myCorrection)/1000
 
-    servo = AngularServo(PIN_A, initial_angle=0, min_angle=-90, max_angle=90, min_pulse_width=minPW, max_pulse_width=maxPW)
-    servo.angle = 0
-    time.sleep(2)
-    servo.angle = -90
-    time.sleep(2)
-    servo.angle = 0
-    time.sleep(2)
-    servo.angle = 90
-    time.sleep(2)
-    servo.angle = 0
+    servo = Servo(PIN_A,min_pulse_width=minPW,max_pulse_width=maxPW)
+
+    while True:
+        servo.mid()
+        print("mid")
+        sleep(0.5)
+        servo.min()
+        print("min")
+        sleep(1)
+        servo.mid()
+        print("mid")
+        sleep(0.5)
+        servo.max()
+        print("max")
+        sleep(1)
 
 if __name__ == '__main__':
     main()
